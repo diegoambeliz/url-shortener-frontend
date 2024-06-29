5e3fbeb39582a39af8e55a6cb9959fce69e2a5b0
Diego Ambeliz <diegoambeliz@gmail.com>
2024-06-28 20:11:16 -0700

    error interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,15 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { httpInterceptor } from './shared/interceptors/http.interceptor';
 import { provideNgVibeDialog } from '@ng-vibe/dialog';
+import { provideNgVibeToastify } from '@ng-vibe/toastify';
+import { errorInterceptor } from './shared/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(withInterceptors([httpInterceptor])),
+    provideHttpClient(withInterceptors([httpInterceptor, errorInterceptor])),
     provideNgVibeDialog(),
+    provideNgVibeToastify()
   ]
 };
--- src/app/shared/interceptors/error.interceptor.ts
+++ src/app/shared/interceptors/error.interceptor.ts
@@ -1,8 +1,38 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { AppearanceAnimation, ProgressBar, TextAlignEnum, ToastPosition, ToastTypeEnum, ToastifyRemoteControl } from '@ng-vibe/toastify';
+import { catchError, throwError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
 
-  console.log(req);
+  const toast: ToastifyRemoteControl = new ToastifyRemoteControl();
 
-  return next(req);
+  toast.options = {
+    title: "Error",
+    autoCloseDuration: 3000,
+    layoutType: ToastTypeEnum.DANGER,
+    position: ToastPosition.BOTTOM_RIGHT,
+    progressBar: ProgressBar.DECREASE,
+    textAlign: TextAlignEnum.START,
+    animationIn: AppearanceAnimation.BOUNCE_IN,
+  }
+
+  return next(req).pipe(catchError((err) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          toast.options.text = "Unauthorized Access"
+        } else {
+          // Handle other HTTP error codes
+          toast.options.text = 'HTTP error: ' + err.error;
+        }
+      } else {
+        // Handle non-HTTP errors
+        toast.options.text = 'An error occurred:' + err.error;
+      }
+
+      if(toast.options.text)
+        toast.openToast();
+
+      // Re-throw the error to propagate it further
+      return throwError(() => err); 
+  }));
 };
